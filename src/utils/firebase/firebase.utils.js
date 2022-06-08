5f00e8d51988928757e0a45343e5cd8ed5fb1553
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-06-08 14:02:50 +0530

    added our sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKm07HOsDgdvqcR_JMDxTrzkPucyYZyyI",
+  authDomain: "dev-clothing-db-6de91.firebaseapp.com",
+  projectId: "dev-clothing-db-6de91",
+  storageBucket: "dev-clothing-db-6de91.appspot.com",
+  messagingSenderId: "968565684352",
+  appId: "1:968565684352:web:31e7c187ed2ba0a69c8e65",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user!", error.message);
+    }
+  }
+
+  return userDocRef;
+};
